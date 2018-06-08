ef9e414eee528ddc38aeb0685ec2bf608645f6a7
MisbahAdil <39994313+MisbahAdil@users.noreply.github.com>
2018-06-08 14:38:35 +0530

    Add files via upload

---
+++ src/Factorial.java
@@ -0,0 +1,20 @@
+
+// FIND FACTORIAL OF A NUMBER.
+
+import java.util.Scanner;
+class Factorial
+{
+  public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+    System.out.println(" ENTER NO : ");
+    int n=sc.nextInt();
+    int fact=1;
+    for(int i=n;i>0;i--)
+     {
+       fact =fact*i;
+     }
+    System.out.println(fact);
+
+}
+}
